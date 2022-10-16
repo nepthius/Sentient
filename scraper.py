41f845e2cfea25cab4e805ab06a5225dc636f57d
Anish Dhanasekaran <98651890+nepthius@users.noreply.github.com>
2022-10-15 20:15:19 -0400

    Scrapped the links from SEC RSS File

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 import json 
+import requests
+from bs4 import BeautifulSoup
+import requests_random_user_agent
 
 company = input("Enter a company: ")
 f = open("company_tickers.json")
@@ -9,3 +12,18 @@ for i, l in data.items():
     currCIK = l['cik_str']
     tickerCIK[currTicker] = currCIK
 tickerCIK[company] 
+
+
+s = requests.Session()
+#print(s)
+url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0000"+ str(tickerCIK[company]) +"&type=10-K%25&dateb=&owner=exclude&start=0&count=40&output=atom"
+#print(url)
+xml = requests.get(url)
+soup = BeautifulSoup(xml.content, 'lxml')
+a = soup.find_all('filing-href')
+
+print(a)
+
+#products = soup.find_all('a', class_ = "klavika simpletextlistitem")
+
+#print(url)
